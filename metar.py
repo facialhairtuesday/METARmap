657302980b896b48d67cadfaaeee7d1301a1f1eb
Marc Stiller <marcstiller@bostondynamics.com>
2022-02-18 14:36:00 -0500

    Added test code

---
+++ metar.py
@@ -0,0 +1,83 @@
+#METARMap V2
+#Author FHT
+#2022-02-18
+
+""" Import Libraries """
+import xml.etree.ElementTree as ET
+import urllib
+import time
+import datetime
+import neopixel as NP
+import board
+
+"""Import Airport List"""
+
+with open("../METARmap/airports") as f:
+    airports = f.readlines()
+airports = [x.strip() for x in airports]
+
+""" LED Configuration """
+LED_COUNT = len(airports) # Number of LEDS --> Equals # of airports in list
+LED_PIN = board.D18 # GPIO pin connected to neopixels (18 uses PWM)
+LED_BRIGHTNESS = 255 # Set to 0 for dimmest, 255 for brightest
+ORDER = NP.GRB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+
+while True:
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((255, 0, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((255, 0, 0, 0))
+    pixels.show()
+    time.sleep(1)
+
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((0, 255, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((0, 255, 0, 0))
+    pixels.show()
+    time.sleep(1)
+
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((0, 0, 255))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((0, 0, 255, 0))
+    pixels.show()
+    time.sleep(1)
+
+    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
