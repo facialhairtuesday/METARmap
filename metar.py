2d0b6165c8a4d6810acd225811a7a3379858f75e
Marc Stiller <marcstiller@bostondynamics.com>
2022-02-22 12:59:27 -0500

    Updated METARmap code to working version

--- metar.py
+++ metar.py
@@ -1,10 +1,10 @@
-#METARMap V2
+#FHT-METAR
 #Author FHT
-#2022-02-18
+#2022-02-22
 
 """ Import Libraries """
 import xml.etree.ElementTree as ET
-import urllib
+import urllib.request
 import time
 import datetime
 import neopixel
@@ -12,72 +12,91 @@ import board
 
 """Import Airport List"""
 
-with open("../METARmap/Airports") as f:
+with open("/home/pi/METARmap/Airports") as f:
     airports = f.readlines()
 airports = [x.strip() for x in airports]
 
 """ LED Configuration """
 LED_COUNT = len(airports) # Number of LEDS --> Equals # of airports in list
 LED_PIN = board.D18 # GPIO pin connected to neopixels (18 uses PWM)
-LED_BRIGHTNESS = 255 # Set to 0 for dimmest, 255 for brightest
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
     LED_PIN, LED_COUNT, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
 
+#print(pixels)
 
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+""" Pixel Colors """
 
+COLOR_VFR = (0,255,0) # Green
+COLOR_MVFR = (0, 0, 255) # Blue
+COLOR_IFR = (255,0,0) # Red
+COLOR_LIFR = (125,0,125) # Magenta
+COLOR_CLEAR = (0,0,0) # CLEAR
 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(LED_COUNT):
-            pixel_index = (i * 256 // LED_COUNT) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-        time.sleep(wait)
+""" Get METAR Info """
 
+url = 'https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=x>
+for airport in airports:
+    url += (airport + ",")
 
-while True:
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((255, 0, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0, 0))
-    pixels.show()
-    time.sleep(1)
+url = url[:-1]
+url = url +(',7B3')
+#print(url)
 
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 255, 0))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0, 0))
-    pixels.show()
-    time.sleep(1)
+try:
+        with urllib.request.urlopen(url) as response:
+                xml_metar = response.read()
+        metar = ET.fromstring(xml_metar)
+except Exceptions:
+        time.sleep(60)
+        pass
 
-    # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 0, 255))
-    # Uncomment this line if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255, 0))
-    pixels.show()
-    time.sleep(1)
+conditionDict = {"NULL": {"flightCategory" : ""}}
+conditionDict.pop("NULL")
 
-    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+stationList = []
+for metarInfo in metar.iter('METAR'):
+        stationID = metarInfo.find('station_id').text
+#       print(stationID)
+        if metarInfo.find('flight_category') is None:
+                continue
+        flightCategory = metarInfo.find('flight_category').text
+#       print(flightCategory)
+        conditionDict[stationID] = flightCategory
+
+#print(conditionDict)
+
+""" Organize METAR Info """
+
+organizedWeather = {}
+for airport in airports:
+        organizedWeather[airport] = "NONE"
+
+for key in organizedWeather:
+        organizedWeather[key] = conditionDict[key]
+#print(conditionDict)
+#print(organizedWeather)
+
+""" Set LED Colors """
+i=0
+for key in organizedWeather:
+        if organizedWeather[key] == "VFR":
+                pixels[i] = COLOR_VFR
+        elif organizedWeather[key] == "MVFR":
+                pixels[i] = COLOR_MVFR
+        elif organizedWeather[key] == "IFR":
+                pixels[i] = COLOR_IFR
+        elif organizedWeather[key] == "LIFR":
+                pixels[i] = COLOR_LIFR
+        else:
+                pixels[i] = COLOR_CLEAR
+        i+=1
+
+for key in organizedWeather:
+        print("At " + key + " the current weather is "
+              + organizedWeather[key])
+
+#print(pixels) # Uncomment to check color assignnment for each LED
+pixels.show()
